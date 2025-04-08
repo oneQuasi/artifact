2728bfc76ef823a797ec7408c305498adfb9a518
Quasi <quasirox74@gmail.com>
2025-04-08 18:27:48 -0400

    Add material eval -------------------------------------------------- -------------------------------------------------- Results of Artifact vs ArtiNoEval (10+0.1, NULL, NULL, UHO_Lichess_4852_v1.epd): Elo: 136.97 +/- 15.68, nElo: 425.52 +/- 43.96 LOS: 100.00 %, DrawRatio: 25.00 %, PairsRatio: inf Games: 240, Wins: 90, Losses: 0, Draws: 150, Points: 165.0 (68.75 %) Ptnml(0-2): [0, 0, 30, 90, 0], WL/DD Ratio: 0.00 LLR: 2.90 (100.2%) (-2.25, 2.89) [0.00, 10.00] -------------------------------------------------- SPRT ([0.00, 10.00]) completed - H1 was accepted Finished match

--- src/search.rs
+++ src/search.rs
@@ -16,8 +16,48 @@ fn team_to_move<T: BitInt>(board: &mut Board<T>) -> i32 {
     }
 }
 
+pub const PAWN: i32 = 100;
+pub const KNIGHT: i32 = 305;
+pub const BISHOP: i32 = 333;
+pub const ROOK: i32 = 563;
+pub const QUEEN: i32 = 950;
+
 fn eval<T: BitInt>(board: &mut Board<T>) -> i32 {
-    0
+    let mut score = 0;
+
+    let pawns = board.state.pieces[0];
+    let knights = board.state.pieces[1];
+    let bishops = board.state.pieces[2];
+    let rooks = board.state.pieces[3];
+    let queens = board.state.pieces[4];
+
+    let white = board.state.white;
+    let black = board.state.black;
+
+    let white_pawns = pawns.and(white).count() as i32;
+    let black_pawns = pawns.and(black).count() as i32;
+
+    let white_knights = knights.and(white).count() as i32;
+    let black_knights = knights.and(black).count() as i32;
+
+    let white_bishops = bishops.and(white).count() as i32;
+    let black_bishops = bishops.and(black).count() as i32;
+
+    let white_rooks = rooks.and(white).count() as i32;
+    let black_rooks = rooks.and(black).count() as i32;
+
+    let white_queens = queens.and(white).count() as i32;
+    let black_queens = queens.and(black).count() as i32;
+
+    score += (white_pawns - black_pawns) * PAWN;
+    score += (white_knights - black_knights) * KNIGHT;
+    score += (white_bishops - black_bishops) * BISHOP;
+    score += (white_rooks - black_rooks) * ROOK;
+    score += (white_queens - black_queens) * QUEEN;
+
+    score *= team_to_move(board);
+
+    score
 }
 
 pub fn search<T: BitInt>(board: &mut Board<T>, info: &mut SearchInfo, depth: i32) -> i32 {
